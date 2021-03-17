0e04740e36606db004cabfe9c03a3a3d66f10aae
francisco <franciscojdsjr@gmail.com>
2021-03-17 18:54:16 -0300

    aula5

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -1,10 +1,4 @@
-
 public class CriaConta {
-
-    double saldo;
-    int agencia;
-    int numero;
-    String titular;
 
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
@@ -15,10 +9,12 @@ public class CriaConta {
         System.out.println(primeiraConta.saldo);
 
         Conta segundaConta = new Conta();
-
         segundaConta.saldo = 50;
 
-        System.out.println("Primeira conta tem " + primeiraConta.saldo);
-        System.out.println("Segunda conta tem " + segundaConta.saldo);
+        System.out.println("primeira conta tem " + primeiraConta.saldo);
+        System.out.println("segunda conta tem " + segundaConta.saldo);
+
+        System.out.println("referenciar: " + primeiraConta);
     }
+
 }
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -1,23 +1,28 @@
 public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
 
-    public static void main(String[] args) {
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
 
-        Conta contaDoPaulo = new Conta();
-        contaDoPaulo.saldo = 100;
-        contaDoPaulo.deposita(50);
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
 
-        System.out.println(contaDoPaulo.saldo);
-        boolean conseguiuRetirar = contaDoPaulo.saca(20);
-        System.out.println(contaDoPaulo.saldo);
-        System.out.println(conseguiuRetirar);
+		boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
 
-        Conta contaDaMarcela = new Conta();
-        contaDaMarcela.deposita(1000);
+		if (sucessoTransferencia) {
+			System.out.println("transferencia com sucesso");
 
-        contaDaMarcela.transfere(300, contaDoPaulo);
-        System.out.println(contaDaMarcela.saldo);
-        System.out.println(contaDoPaulo.saldo);
+		} else {
+			System.out.println("faltou dinheiro");
 
-    }
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
+	}
 
 }
